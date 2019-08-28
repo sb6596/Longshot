RevengeOS/Screenshot

Add edit action into notification

smali/org/pixelexperience/screenshot/EditActionInjector.smali
@@ -1,0 +1,96 @@
+.class public Lorg/pixelexperience/screenshot/EditActionInjector;
+.super Ljava/lang/Object;
+.source "EditActionInjector.java"
+
+
+# direct methods
+.method public constructor <init>()V
+    .locals 0
+
+    .line 10
+    invoke-direct {p0}, Ljava/lang/Object;-><init>()V
+
+    return-void
+.end method
+
+.method public static getAction(Landroid/net/Uri;Landroid/content/Context;)Landroid/app/Notification$Action;
+    .locals 6
+    .param p0, "uri"    # Landroid/net/Uri;
+    .param p1, "context"    # Landroid/content/Context;
+
+    .line 12
+    invoke-virtual {p1}, Landroid/content/Context;->getResources()Landroid/content/res/Resources;
+
+    move-result-object v0
+
+    .line 13
+    .local v0, "r":Landroid/content/res/Resources;
+    new-instance v1, Landroid/content/Intent;
+
+    const-string v2, "android.intent.action.EDIT"
+
+    invoke-direct {v1, v2}, Landroid/content/Intent;-><init>(Ljava/lang/String;)V
+
+    .line 14
+    .local v1, "editIntent":Landroid/content/Intent;
+    const-string v2, "image/jpeg"
+
+    invoke-virtual {v1, v2}, Landroid/content/Intent;->setType(Ljava/lang/String;)Landroid/content/Intent;
+
+    .line 15
+    invoke-virtual {v1, p0}, Landroid/content/Intent;->setData(Landroid/net/Uri;)Landroid/content/Intent;
+
+    .line 16
+    const/4 v2, 0x1
+
+    invoke-virtual {v1, v2}, Landroid/content/Intent;->addFlags(I)Landroid/content/Intent;
+
+    .line 17
+    const/4 v3, 0x2
+
+    invoke-virtual {v1, v3}, Landroid/content/Intent;->addFlags(I)Landroid/content/Intent;
+
+    .line 18
+    new-instance v3, Landroid/content/Intent;
+
+    const-class v4, Lorg/pixelexperience/screenshot/GlobalScreenshot$ScreenshotActionReceiver;
+
+    invoke-direct {v3, p1, v4}, Landroid/content/Intent;-><init>(Landroid/content/Context;Ljava/lang/Class;)V
+
+    .line 20
+    const-string v4, "android:screenshot_uri_id"
+
+    invoke-virtual {v3, v4, v1}, Landroid/content/Intent;->putExtra(Ljava/lang/String;Landroid/os/Parcelable;)Landroid/content/Intent;
+
+    move-result-object v3
+
+    .line 18
+    const/high16 v4, 0x10000000
+
+    invoke-static {p1, v2, v3, v4}, Landroid/app/PendingIntent;->getBroadcast(Landroid/content/Context;ILandroid/content/Intent;I)Landroid/app/PendingIntent;
+
+    move-result-object v2
+
+    .line 22
+    .local v2, "editAction":Landroid/app/PendingIntent;
+    new-instance v3, Landroid/app/Notification$Action$Builder;
+
+    .line 24
+    const v4, 0x7f0c0021
+
+    invoke-virtual {v0, v4}, Landroid/content/res/Resources;->getString(I)Ljava/lang/String;
+
+    move-result-object v4
+
+    const v5, 0x7f020035
+
+    invoke-direct {v3, v5, v4, v2}, Landroid/app/Notification$Action$Builder;-><init>(ILjava/lang/CharSequence;Landroid/app/PendingIntent;)V
+
+    .line 25
+    .local v3, "editActionBuilder":Landroid/app/Notification$Action$Builder;
+    invoke-virtual {v3}, Landroid/app/Notification$Action$Builder;->build()Landroid/app/Notification$Action;
+
+    move-result-object v4
+
+    return-object v4
+.end method

smali/org/pixelexperience/screenshot/GlobalScreenshot$ScreenshotActionReceiver.smali
@@ -1,0 +1,171 @@
+.class public Lorg/pixelexperience/screenshot/GlobalScreenshot$ScreenshotActionReceiver;
+.super Landroid/content/BroadcastReceiver;
+.source "GlobalScreenshot.java"
+
+
+# annotations
+.annotation system Ldalvik/annotation/EnclosingClass;
+    value = Lorg/pixelexperience/screenshot/GlobalScreenshot;
+.end annotation
+
+.annotation system Ldalvik/annotation/InnerClass;
+    accessFlags = 0x9
+    name = "ScreenshotActionReceiver"
+.end annotation
+
+
+# direct methods
+.method public constructor <init>()V
+    .locals 0
+
+    .line 901
+    invoke-direct {p0}, Landroid/content/BroadcastReceiver;-><init>()V
+
+    return-void
+.end method
+
+
+# virtual methods
+.method public onReceive(Landroid/content/Context;Landroid/content/Intent;)V
+    .locals 4
+
+    .line 905
+    :try_start_0
+    invoke-static {}, Landroid/app/ActivityManager;->getService()Landroid/app/IActivityManager;
+
+    move-result-object v0
+
+    const-string v1, "screenshot"
+
+    invoke-interface {v0, v1}, Landroid/app/IActivityManager;->closeSystemDialogs(Ljava/lang/String;)V
+    :try_end_0
+    .catch Landroid/os/RemoteException; {:try_start_0 .. :try_end_0} :catch_0
+
+    .line 907
+    goto :goto_0
+
+    .line 906
+    :catch_0
+    move-exception v0
+
+    .line 909
+    :goto_0
+    const-string v0, "android:screenshot_uri_id"
+
+    invoke-virtual {p2, v0}, Landroid/content/Intent;->getParcelableExtra(Ljava/lang/String;)Landroid/os/Parcelable;
+
+    move-result-object p2
+
+    check-cast p2, Landroid/content/Intent;
+
+    .line 912
+    invoke-virtual {p1}, Landroid/content/Context;->getResources()Landroid/content/res/Resources;
+
+    move-result-object v0
+
+    const v1, 0x7f0c0022
+
+    invoke-virtual {v0, v1}, Landroid/content/res/Resources;->getString(I)Ljava/lang/String;
+
+    move-result-object v0
+
+    .line 913
+    invoke-virtual {p2}, Landroid/content/Intent;->getAction()Ljava/lang/String;
+
+    move-result-object v1
+
+    const-string v2, "android.intent.action.EDIT"
+
+    const/4 v3, 0x1
+
+    if-ne v1, v2, :cond_0
+
+    if-eqz v0, :cond_0
+
+    .line 914
+    invoke-virtual {v0}, Ljava/lang/String;->length()I
+
+    move-result v1
+
+    if-lez v1, :cond_0
+
+    .line 915
+    invoke-static {v0}, Landroid/content/ComponentName;->unflattenFromString(Ljava/lang/String;)Landroid/content/ComponentName;
+
+    move-result-object v0
+
+    invoke-virtual {p2, v0}, Landroid/content/Intent;->setComponent(Landroid/content/ComponentName;)Landroid/content/Intent;
+
+    .line 916
+    const-string v0, "notification"
+
+    .line 917
+    invoke-virtual {p1, v0}, Landroid/content/Context;->getSystemService(Ljava/lang/String;)Ljava/lang/Object;
+
+    move-result-object v0
+
+    check-cast v0, Landroid/app/NotificationManager;
+
+    .line 918
+    invoke-virtual {v0, v3}, Landroid/app/NotificationManager;->cancel(I)V
+
+    .line 919
+    goto :goto_1
+
+    .line 920
+    :cond_0
+    const/4 v0, 0x0
+
+    new-instance v1, Landroid/content/Intent;
+
+    const-class v2, Lorg/pixelexperience/screenshot/GlobalScreenshot$TargetChosenReceiver;
+
+    invoke-direct {v1, p1, v2}, Landroid/content/Intent;-><init>(Landroid/content/Context;Ljava/lang/Class;)V
+
+    const/high16 v2, 0x50000000
+
+    invoke-static {p1, v0, v1, v2}, Landroid/app/PendingIntent;->getBroadcast(Landroid/content/Context;ILandroid/content/Intent;I)Landroid/app/PendingIntent;
+
+    move-result-object v0
+
+    .line 923
+    const/4 v1, 0x0
+
+    .line 924
+    invoke-virtual {v0}, Landroid/app/PendingIntent;->getIntentSender()Landroid/content/IntentSender;
+
+    move-result-object v0
+
+    .line 923
+    invoke-static {p2, v1, v0}, Landroid/content/Intent;->createChooser(Landroid/content/Intent;Ljava/lang/CharSequence;Landroid/content/IntentSender;)Landroid/content/Intent;
+
+    move-result-object p2
+
+    const v0, 0x10008000
+
+    .line 925
+    invoke-virtual {p2, v0}, Landroid/content/Intent;->addFlags(I)Landroid/content/Intent;
+
+    move-result-object p2
+
+    .line 928
+    :goto_1
+    invoke-static {}, Landroid/app/ActivityOptions;->makeBasic()Landroid/app/ActivityOptions;
+
+    move-result-object v0
+
+    .line 929
+    invoke-virtual {v0, v3}, Landroid/app/ActivityOptions;->setDisallowEnterPictureInPictureWhileLaunching(Z)V
+
+    .line 931
+    invoke-virtual {v0}, Landroid/app/ActivityOptions;->toBundle()Landroid/os/Bundle;
+
+    move-result-object v0
+
+    sget-object v1, Landroid/os/UserHandle;->CURRENT:Landroid/os/UserHandle;
+
+    invoke-virtual {p1, p2, v0, v1}, Landroid/content/Context;->startActivityAsUser(Landroid/content/Intent;Landroid/os/Bundle;Landroid/os/UserHandle;)V
+
+    .line 932
+    return-void
+.end method

smali/org/pixelexperience/screenshot/SaveImageInBackgroundTask.smali
@@ -1344,6 +1344,22 @@
 
     invoke-virtual {v8, v15}, Landroid/app/Notification$Builder;->addAction(Landroid/app/Notification$Action;)Landroid/app/Notification$Builder;
 
+    # Edit action begins
+    iget-object v8, v1, Lorg/pixelexperience/screenshot/SaveImageInBackgroundTask;->mNotificationBuilder:Landroid/app/Notification$Builder;
+
+    #sget-object v7, Landroid/provider/MediaStore$Images$Media;->EXTERNAL_CONTENT_URI:Landroid/net/Uri;
+    #invoke-virtual {v2, v7, v0}, Landroid/content/ContentResolver;->insert(Landroid/net/Uri;Landroid/content/ContentValues;)Landroid/net/Uri;
+    #move-result-object v7
+    #local v4, "context":Landroid/content/Context;
+
+    invoke-static {v7, v4}, Lorg/pixelexperience/screenshot/EditActionInjector;->getAction(Landroid/net/Uri;Landroid/content/Context;)Landroid/app/Notification$Action;
+
+    move-result-object v15
+
+    invoke-virtual {v8, v15}, Landroid/app/Notification$Builder;->addAction(Landroid/app/Notification$Action;)Landroid/app/Notification$Builder;
+
+    # Edit action ends
+
     .line 455
     new-instance v8, Landroid/content/Intent;
 

sb6596/Longshot

smali/org/aospextended/screenshot/EditActionInjector.smali
@@ -1,0 +1,96 @@
+.class public Lorg/aospextended/screenshot/EditActionInjector;
+.super Ljava/lang/Object;
+.source "EditActionInjector.java"
+
+
+# direct methods
+.method public constructor <init>()V
+    .locals 0
+
+    .line 10
+    invoke-direct {p0}, Ljava/lang/Object;-><init>()V
+
+    return-void
+.end method
+
+.method public static getAction(Landroid/net/Uri;Landroid/content/Context;)Landroid/app/Notification$Action;
+    .locals 6
+    .param p0, "uri"    # Landroid/net/Uri;
+    .param p1, "context"    # Landroid/content/Context;
+
+    .line 12
+    invoke-virtual {p1}, Landroid/content/Context;->getResources()Landroid/content/res/Resources;
+
+    move-result-object v0
+
+    .line 13
+    .local v0, "r":Landroid/content/res/Resources;
+    new-instance v1, Landroid/content/Intent;
+
+    const-string v2, "android.intent.action.EDIT"
+
+    invoke-direct {v1, v2}, Landroid/content/Intent;-><init>(Ljava/lang/String;)V
+
+    .line 14
+    .local v1, "editIntent":Landroid/content/Intent;
+    const-string v2, "image/jpeg"
+
+    invoke-virtual {v1, v2}, Landroid/content/Intent;->setType(Ljava/lang/String;)Landroid/content/Intent;
+
+    .line 15
+    invoke-virtual {v1, p0}, Landroid/content/Intent;->setData(Landroid/net/Uri;)Landroid/content/Intent;
+
+    .line 16
+    const/4 v2, 0x1
+
+    invoke-virtual {v1, v2}, Landroid/content/Intent;->addFlags(I)Landroid/content/Intent;
+
+    .line 17
+    const/4 v3, 0x2
+
+    invoke-virtual {v1, v3}, Landroid/content/Intent;->addFlags(I)Landroid/content/Intent;
+
+    .line 18
+    new-instance v3, Landroid/content/Intent;
+
+    const-class v4, Lorg/aospextended/screenshot/GlobalScreenshot$ScreenshotActionReceiver;
+
+    invoke-direct {v3, p1, v4}, Landroid/content/Intent;-><init>(Landroid/content/Context;Ljava/lang/Class;)V
+
+    .line 20
+    const-string v4, "android:screenshot_uri_id"
+
+    invoke-virtual {v3, v4, v1}, Landroid/content/Intent;->putExtra(Ljava/lang/String;Landroid/os/Parcelable;)Landroid/content/Intent;
+
+    move-result-object v3
+
+    .line 18
+    const/high16 v4, 0x10000000
+
+    invoke-static {p1, v2, v3, v4}, Landroid/app/PendingIntent;->getBroadcast(Landroid/content/Context;ILandroid/content/Intent;I)Landroid/app/PendingIntent;
+
+    move-result-object v2
+
+    .line 22
+    .local v2, "editAction":Landroid/app/PendingIntent;
+    new-instance v3, Landroid/app/Notification$Action$Builder;
+
+    .line 24
+    const v4, 0x7f0c0021
+
+    invoke-virtual {v0, v4}, Landroid/content/res/Resources;->getString(I)Ljava/lang/String;
+
+    move-result-object v4
+
+    const v5, 0x7f020035
+
+    invoke-direct {v3, v5, v4, v2}, Landroid/app/Notification$Action$Builder;-><init>(ILjava/lang/CharSequence;Landroid/app/PendingIntent;)V
+
+    .line 25
+    .local v3, "editActionBuilder":Landroid/app/Notification$Action$Builder;
+    invoke-virtual {v3}, Landroid/app/Notification$Action$Builder;->build()Landroid/app/Notification$Action;
+
+    move-result-object v4
+
+    return-object v4
+.end method

smali/org/aospextended/screenshot/GlobalScreenshot$ScreenshotActionReceiver.smali
@@ -1,0 +1,171 @@
+.class public Lorg/aospextended/screenshot/GlobalScreenshot$ScreenshotActionReceiver;
+.super Landroid/content/BroadcastReceiver;
+.source "GlobalScreenshot.java"
+
+
+# annotations
+.annotation system Ldalvik/annotation/EnclosingClass;
+    value = Lorg/aospextended/screenshot/GlobalScreenshot;
+.end annotation
+
+.annotation system Ldalvik/annotation/InnerClass;
+    accessFlags = 0x9
+    name = "ScreenshotActionReceiver"
+.end annotation
+
+
+# direct methods
+.method public constructor <init>()V
+    .locals 0
+
+    .line 901
+    invoke-direct {p0}, Landroid/content/BroadcastReceiver;-><init>()V
+
+    return-void
+.end method
+
+
+# virtual methods
+.method public onReceive(Landroid/content/Context;Landroid/content/Intent;)V
+    .locals 4
+
+    .line 905
+    :try_start_0
+    invoke-static {}, Landroid/app/ActivityManager;->getService()Landroid/app/IActivityManager;
+
+    move-result-object v0
+
+    const-string v1, "screenshot"
+
+    invoke-interface {v0, v1}, Landroid/app/IActivityManager;->closeSystemDialogs(Ljava/lang/String;)V
+    :try_end_0
+    .catch Landroid/os/RemoteException; {:try_start_0 .. :try_end_0} :catch_0
+
+    .line 907
+    goto :goto_0
+
+    .line 906
+    :catch_0
+    move-exception v0
+
+    .line 909
+    :goto_0
+    const-string v0, "android:screenshot_uri_id"
+
+    invoke-virtual {p2, v0}, Landroid/content/Intent;->getParcelableExtra(Ljava/lang/String;)Landroid/os/Parcelable;
+
+    move-result-object p2
+
+    check-cast p2, Landroid/content/Intent;
+
+    .line 912
+    invoke-virtual {p1}, Landroid/content/Context;->getResources()Landroid/content/res/Resources;
+
+    move-result-object v0
+
+    const v1, 0x7f0c0022
+
+    invoke-virtual {v0, v1}, Landroid/content/res/Resources;->getString(I)Ljava/lang/String;
+
+    move-result-object v0
+
+    .line 913
+    invoke-virtual {p2}, Landroid/content/Intent;->getAction()Ljava/lang/String;
+
+    move-result-object v1
+
+    const-string v2, "android.intent.action.EDIT"
+
+    const/4 v3, 0x1
+
+    if-ne v1, v2, :cond_0
+
+    if-eqz v0, :cond_0
+
+    .line 914
+    invoke-virtual {v0}, Ljava/lang/String;->length()I
+
+    move-result v1
+
+    if-lez v1, :cond_0
+
+    .line 915
+    invoke-static {v0}, Landroid/content/ComponentName;->unflattenFromString(Ljava/lang/String;)Landroid/content/ComponentName;
+
+    move-result-object v0
+
+    invoke-virtual {p2, v0}, Landroid/content/Intent;->setComponent(Landroid/content/ComponentName;)Landroid/content/Intent;
+
+    .line 916
+    const-string v0, "notification"
+
+    .line 917
+    invoke-virtual {p1, v0}, Landroid/content/Context;->getSystemService(Ljava/lang/String;)Ljava/lang/Object;
+
+    move-result-object v0
+
+    check-cast v0, Landroid/app/NotificationManager;
+
+    .line 918
+    invoke-virtual {v0, v3}, Landroid/app/NotificationManager;->cancel(I)V
+
+    .line 919
+    goto :goto_1
+
+    .line 920
+    :cond_0
+    const/4 v0, 0x0
+
+    new-instance v1, Landroid/content/Intent;
+
+    const-class v2, Lorg/aospextended/screenshot/GlobalScreenshot$TargetChosenReceiver;
+
+    invoke-direct {v1, p1, v2}, Landroid/content/Intent;-><init>(Landroid/content/Context;Ljava/lang/Class;)V
+
+    const/high16 v2, 0x50000000
+
+    invoke-static {p1, v0, v1, v2}, Landroid/app/PendingIntent;->getBroadcast(Landroid/content/Context;ILandroid/content/Intent;I)Landroid/app/PendingIntent;
+
+    move-result-object v0
+
+    .line 923
+    const/4 v1, 0x0
+
+    .line 924
+    invoke-virtual {v0}, Landroid/app/PendingIntent;->getIntentSender()Landroid/content/IntentSender;
+
+    move-result-object v0
+
+    .line 923
+    invoke-static {p2, v1, v0}, Landroid/content/Intent;->createChooser(Landroid/content/Intent;Ljava/lang/CharSequence;Landroid/content/IntentSender;)Landroid/content/Intent;
+
+    move-result-object p2
+
+    const v0, 0x10008000
+
+    .line 925
+    invoke-virtual {p2, v0}, Landroid/content/Intent;->addFlags(I)Landroid/content/Intent;
+
+    move-result-object p2
+
+    .line 928
+    :goto_1
+    invoke-static {}, Landroid/app/ActivityOptions;->makeBasic()Landroid/app/ActivityOptions;
+
+    move-result-object v0
+
+    .line 929
+    invoke-virtual {v0, v3}, Landroid/app/ActivityOptions;->setDisallowEnterPictureInPictureWhileLaunching(Z)V
+
+    .line 931
+    invoke-virtual {v0}, Landroid/app/ActivityOptions;->toBundle()Landroid/os/Bundle;
+
+    move-result-object v0
+
+    sget-object v1, Landroid/os/UserHandle;->CURRENT:Landroid/os/UserHandle;
+
+    invoke-virtual {p1, p2, v0, v1}, Landroid/content/Context;->startActivityAsUser(Landroid/content/Intent;Landroid/os/Bundle;Landroid/os/UserHandle;)V
+
+    .line 932
+    return-void
+.end method

smali/org/aospextended/screenshot/SaveImageInBackgroundTask.smali
@@ -1344,6 +1344,22 @@
 
     invoke-virtual {v8, v15}, Landroid/app/Notification$Builder;->addAction(Landroid/app/Notification$Action;)Landroid/app/Notification$Builder;
 
+    # Edit action begins
+    iget-object v8, v1, Lorg/aospextended/screenshot/SaveImageInBackgroundTask;->mNotificationBuilder:Landroid/app/Notification$Builder;
+
+    #sget-object v7, Landroid/provider/MediaStore$Images$Media;->EXTERNAL_CONTENT_URI:Landroid/net/Uri;
+    #invoke-virtual {v2, v7, v0}, Landroid/content/ContentResolver;->insert(Landroid/net/Uri;Landroid/content/ContentValues;)Landroid/net/Uri;
+    #move-result-object v7
+    #local v4, "context":Landroid/content/Context;
+
+    invoke-static {v7, v4}, Lorg/aospextended/screenshot/EditActionInjector;->getAction(Landroid/net/Uri;Landroid/content/Context;)Landroid/app/Notification$Action;
+
+    move-result-object v15
+
+    invoke-virtual {v8, v15}, Landroid/app/Notification$Builder;->addAction(Landroid/app/Notification$Action;)Landroid/app/Notification$Builder;
+
+    # Edit action ends
+
     .line 455
     new-instance v8, Landroid/content/Intent;
 